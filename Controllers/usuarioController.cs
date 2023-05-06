f8f3ac0581ec5637d2941965a06ce31d05ac88de
ing-jul-i-am <115831786+ing-jul-i-am@users.noreply.github.com>
2023-05-06 04:30:58 -0600

    Adicion de la funcion de Actualizar usuario y tambien cambie el nombre de las URLs para que no tenga localhost sino el nombre de la maquina.

--- Controllers/usuarioController.cs
+++ Controllers/usuarioController.cs
@@ -28,10 +28,10 @@ namespace app_Vista_Usuario.Controllers
             //La direccion de nuestra API
             if (idUsuario==null)
             {
-                url = $"http://localhost/API-USUARIO/rest/api/listarUsuarios";
+                url = $"http://JULIAN/API-USUARIO/rest/api/listarUsuarios";
             } else
             {
-                url = $"http://localhost/API-USUARIO/rest/api/listarUsuariosPorID?idUsuario=" + idUsuario;
+                url = $"http://JULIAN/API-USUARIO/rest/api/listarUsuariosPorID?idUsuario=" + idUsuario;
             }
             
 
@@ -86,7 +86,7 @@ namespace app_Vista_Usuario.Controllers
             json = JsonConvert.SerializeObject(insertar);
 
             WebClient webClient = new WebClient();
-            string url = $"http://localhost/API-USUARIO/rest/api/insertarUsuario";
+            string url = $"http://JULIAN/API-USUARIO/rest/api/insertarUsuario";
             var request = (HttpWebRequest)WebRequest.Create(url);
 
             ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
@@ -104,6 +104,88 @@ namespace app_Vista_Usuario.Controllers
                 return RedirectToAction("Usuario", "Usuario");
             }
             return RedirectToAction("newUsuario", "Usuario");
+        }
+
+        public ActionResult ActualizarUsuario(string idUsuario)
+        {
+            DataSet dsi = new DataSet();
+            var url = "";
+
+            //La direccion de nuestra API
+            if (idUsuario == null)
+            {
+                url = $"http://JULIAN/API-USUARIO/rest/api/listarUsuarios";
+            }
+            else
+            {
+                url = $"http://JULIAN/API-USUARIO/rest/api/listarUsuariosPorID?idUsuario=" + idUsuario;
+            }
+
+
+            //La variable que guarda la peticion al API
+            var request = (HttpWebRequest)WebRequest.Create(url);
+            request.Method = "GET";
+            request.ContentType = "application/json";
+            //Aqui se pedirian los permisos y autenticacion con la API
+            request.Accept = "application/json";
+
+            string responseBody;
+
+            //Aqui se ejecuta la peticion a la API que nos devuelve un json
+            //inicialmente lo guardamos en el string 'responseBody'
+            //y por ultimo lo convertimos en un dataset 'dsi' ya que tiene la estructura json
+            try
+            {
+                using (WebResponse response = request.GetResponse())
+                {
+                    using (Stream strReader = response.GetResponseStream())
+                    {
+                        using (StreamReader objReader = new StreamReader(strReader))
+                        {
+                            responseBody = objReader.ReadToEnd();
+                        }
+                    }
+                    dsi = JsonConvert.DeserializeObject<DataSet>(responseBody);
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return View(dsi);
+        }
+
+        public ActionResult Actualizar(FormCollection formCollection)
+        {
+            string json, resultJson;
+            Byte[] reqString, resByte;
+            requestUsuario insertar = new requestUsuario();
+            insertar.idUsuario = Convert.ToInt32(formCollection["idUsuario"]);
+            insertar.nombre = formCollection["nombre"];
+            insertar.contrasena = formCollection["contrasena"];
+
+            json = JsonConvert.SerializeObject(insertar);
+
+            WebClient webClient = new WebClient();
+            string url = $"http://JULIAN/API-USUARIO/rest/api/actualizarUsuario";
+            var request = (HttpWebRequest)WebRequest.Create(url);
+
+            ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
+            webClient.Headers["content-type"] = "application/json";
+            reqString = Encoding.UTF8.GetBytes(json);
+            resByte = webClient.UploadData(request.Address.ToString(), "post", reqString);
+            resultJson = Encoding.UTF8.GetString(resByte);
+
+            responseUsuario result = new responseUsuario();
+            result = JsonConvert.DeserializeObject<responseUsuario>(resultJson);
+            webClient.Dispose();
+
+            if (result.respuesta == 1)
+            {
+                return RedirectToAction("Usuario", "Usuario");
+            }
+            return RedirectToAction("ActualizarUsuario", "Usuario");
         }
     }
 }
---
+++ js/jsUsuario.js
@@ -0,0 +1,14 @@
+﻿function validarGuardado() {
+    let respuesta = "";
+    if ($("#idUsuario").val().trim() == "")
+        respuesta += "\n{idUsuario}";
+
+    if ($("#nombre").val().trim() == "")
+        respuesta += "\n{nombre}";
+
+    if ($("#contrasena").val().trim() == "")
+        respuesta += "\n{contrasena}";
+
+    if (respuesta != "")
+        alert("Los siguientes campos no pueden estar vacios: " + respuesta);
+}
